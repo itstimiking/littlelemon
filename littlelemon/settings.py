eef8f1ca3a2e81a63a751f506e0261f04173716d
timiambaye <itstimiking@gmail.com>
2024-04-03 19:35:50 +0100

    Restaurant app created

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'restaurant'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restaurant',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'xxxxxx',
     }
 }
 
